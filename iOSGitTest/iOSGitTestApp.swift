d313a2527ecbfff81101a3ada76e69df09ed2a6f
sebastian narvaez <sebatiannarvaez0553@gmail.com>
2023-08-22 12:30:51 -0500

    Initial Commit

---
+++ iOSGitTest/iOSGitTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  iOSGitTestApp.swift
+//  iOSGitTest
+//
+//  Created by Sebastian Narvaez Baron on 22/08/23.
+//
+
+import SwiftUI
+
+@main
+struct iOSGitTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
