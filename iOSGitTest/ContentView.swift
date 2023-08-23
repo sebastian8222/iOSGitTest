09b5cf8c188a132b53f858066e2c4b687b7a8043
sebastian narvaez <sebatiannarvaez0553@gmail.com>
2023-08-22 20:43:20 -0500

    version 1.0

--- iOSGitTest/ContentView.swift
+++ iOSGitTest/ContentView.swift
@@ -10,12 +10,18 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         VStack {
-            Image(systemName: "globe")
+            Image(systemName: "1.square")
+                .resizable()
                 .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+                .scaledToFit()
+                //.foregroundColor(.accentColor)
+            Text("V 1.0")
+                .font(.largeTitle)
+                .fontWeight(.black)
         }
         .padding()
+        
+        .foregroundColor(.accentColor)
     }
 }
 
